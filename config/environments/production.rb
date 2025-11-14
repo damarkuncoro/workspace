5328d99688e18826a28b77bf59080488e54553bb
Damar Kuncoro <damarkuncoro@gmail.com>
2025-11-14 10:00:33 +0000

    fix(config): update production hosts and database configuration
    - Add allowed hosts for production environment
    - Simplify database.yml configuration and use environment variables
    - Remove executable permissions from bin scripts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,10 @@ Rails.application.configure do
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
 
+  config.hosts = [
+    "tickets.cakramedia.net.id",
+    "/.*\.tickets\.cakramedia\.net\.id/"
+  ]
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
