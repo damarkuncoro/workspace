61292c9cbf6a2a663dc801e9fce8daaef83944b3
Damar Kuncoro <damarkuncoro@gmail.com>
2025-11-22 04:23:23 +0000

    feat(people): add CRUD views and controller for protected people
    Add index, show, new, edit views and corresponding controller actions
    Remove unused columns from customer_devices table

---
+++ app/controllers/protected/people_controller.rb
@@ -0,0 +1,48 @@
+class Protected::PeopleController < Protected::BaseController
+  before_action :set_person, only: %i[show edit update destroy]
+
+  def index
+    @people = Person.order(created_at: :desc)
+  end
+
+  def show
+  end
+
+  def new
+    @person = current_account.person || current_account.build_person
+  end
+
+  def create
+    @person = current_account.person || current_account.build_person
+    if @person.update(person_params)
+      redirect_to protected_person_path(@person), notice: "Person was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @person.update(person_params)
+      redirect_to protected_person_path(@person), notice: "Person was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @person.destroy
+    redirect_to protected_people_path, notice: "Person was successfully destroyed."
+  end
+
+  private
+  def set_person
+    @person = Person.find(params[:id])
+  end
+
+  def person_params
+    params.require(:person).permit(:name, :date_of_birth, :metadata)
+  end
+end
